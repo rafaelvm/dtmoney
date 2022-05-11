c171c1bbf0423aae85948e58b136130bead88438
Rafael Vieira <rafael.vieira@conexasaude.com.br>
2022-05-10 22:11:51 -0300

    chore: created header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({
+  onOpenNewTransactionModal,
+}) => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  );
+};
